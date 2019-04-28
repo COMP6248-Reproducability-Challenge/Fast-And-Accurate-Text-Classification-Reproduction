45a6d9066f54c9fdb22825ed5945296633904294
Feng Xie <xfffrank@gmail.com>
2019-04-28 21:19:00 +0100

    add two models || first commit

---
+++ networks.py
@@ -0,0 +1,169 @@
+'''
+Networks architectures
+'''
+from torch import nn
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class CNN_LSTM(nn.Module):
+    '''Encoder
+    Embedding -> Convolutional layer -> One-layer LSTM
+    '''
+    def __init__(self, input_dim, embedding_dim, ker_size, n_filters, hidden_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.conv = nn.Conv2d(in_channels=1, out_channels=n_filters, kernel_size=(ker_size, embedding_dim))
+        self.lstm = nn.LSTM(input_size=n_filters, hidden_size=hidden_dim)
+        self.dropout = nn.Dropout(p=0.1)
+        self.relu = nn.ReLU()
+   
+    def forward(self, text, h_0):
+        # CNN and LSTM network
+        '''
+        At every time step, the model reads one chunk which has a size of 20 words.
+
+        --- input & output dimension ---
+        
+        Input text: 1 * 20
+        
+        **Embedding**
+        1.Input: 1 * 20
+        2.Output: 1 * 20 * 100
+        
+        **CNN**
+        1. Input(minibatch×in_channels×iH×iW): 1 * 1 * 20 * 100
+        2. Output(minibatch×out_channels×oH×oW): 1 * 128 * 16 * 1
+        
+        **LSTM**
+        1. Inputs: input, (h_0, c_0)
+        input(seq_len, batch, input_size): (16, 1 , 128)
+        c_0(num_layers * num_directions, batch, hidden_size): (1 * 1, 1, 128)
+        h_0(num_layers * num_directions, batch, hidden_size): (1 * 1, 1, 128)
+        2. Outputs: output, (h_n, c_n)
+        output:
+        h_n(num_layers * num_directions, batch, hidden_size): (1 * 1, 1, 128)
+        
+
+        '''
+        embedded = self.embedding(text)
+        #print(embeded.size())
+        conved = self.relu(self.conv(embedded.unsqueeze(1)))  # 1 * 128 * 16 * 1
+        #print(conved.size())
+        # conv -> relu -> dropout
+        conved = self.dropout(conved)
+        conved = conved.squeeze(3)  # 1 * 128 * 16
+        conved = torch.transpose(conved, 1, 2)  # 1 * 16 * 128
+        conved = torch.transpose(conved, 1, 0)  # 16 * 1 * 128
+        c_0 = torch.zeros([1, 1, 128]).to(device)
+        output, (hidden, cell) = self.lstm(conved, (h_0, c_0))
+        ht = hidden.squeeze(0)  # 1 * 128
+        return ht
+
+
+class Policy_S(nn.Module):
+    '''Stopping module
+
+    Three hidden-layer MLP with 128 hidden units per layer.
+    '''
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()    
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(p=0.1)
+        self.relu = nn.ReLU()
+          
+    def forward(self, ht):
+        out = self.fc1(ht)
+        out = self.dropout(out)
+        out = self.relu(out)
+        
+        out = self.fc2(out)
+        out = self.dropout(out)
+        out = self.relu(out)
+        
+        out = self.fc3(out)
+        out = self.dropout(out)
+        out = self.relu(out)
+        out = self.fc4(out)
+        out = torch.sigmoid(out)
+        return out
+    
+
+class Policy_N(nn.Module):
+    '''Re-reading and skipping module
+    
+    Three hidden-layer MLP with 128 hidden units per layer.
+    '''
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()    
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(p=0.1)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+         
+    def forward(self, ht):
+        out = self.fc1(ht)
+        out = self.dropout(out)
+        out = self.relu(out)
+        
+        out = self.fc2(out)
+        out = self.dropout(out)
+        out = self.relu(out)
+        
+        out = self.fc3(out)
+        out = self.dropout(out)
+        out = self.relu(out)
+        out = self.fc4(out)
+        out = self.softmax(out)
+        return out
+    
+
+class Policy_C(nn.Module):
+    '''Classifier
+    
+    Single-layer MLP with 128 hidden units.
+    '''
+
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        #self.fc = nn.Linear(input_dim, output_dim)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.dropout = nn.Dropout(p=0.1)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+         
+    def forward(self, ht):
+        #return self.fc(ht)
+        out = self.fc1(ht)
+        out = self.dropout(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+class ValueNetwork(nn.Module):
+    '''Baseline
+    Reduce the variance.
+
+    Single-layer MLP with 128 hidden units.
+    '''
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        #self.fc = nn.Linear(input_dim, output_dim)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.dropout = nn.Dropout(p=0.1)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+        
+    def forward(self, ht):
+        out = self.fc1(ht)
+        out = self.dropout(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
---
+++ utils.py
@@ -0,0 +1,256 @@
+import time
+import torch
+import torch.nn.functional as F
+from torch.distributions import Bernoulli, Multinomial, Categorical
+import numpy as np
+
+# set up parameters
+CHUNCK_SIZE = 20
+BATCH_SIZE = 1
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+'''Compute FLOPs(Floating point operations) of the models.
+Specify the computational costs for the models above
+
+cnn_cost: CNN model(which is separated from CNN_LSTM above)
+s_cost: policy s(stopping module)
+c_cost: policy c(classifier)
+lstm_cost: LSTM model(which is separated from CNN_LSTM above)
+'''
+cnn_cost = 1024000
+s_cost = 50050
+c_cost = 16770 
+n_cost = 50310
+lstm_cost = 286720
+clstm_cost = cnn_cost + lstm_cost
+
+
+def sample_policy_s(ht, policy_s):
+    '''
+    Draw a stopping decision from a Bernoulli distribution specified by policy s.
+    '''
+    s_prob = policy_s(ht)
+    m = Bernoulli(s_prob)
+    stop_decision = m.sample()
+    # compute the log prob
+    log_prob_s = m.log_prob(stop_decision)
+    return stop_decision, log_prob_s
+
+def sample_policy_c(output_c):
+    '''
+    Draw a label from a multinomial distribution specified by policy c.
+    '''
+    prob_c = F.softmax(output_c, dim=1)
+    m = Categorical(prob_c)
+    pred_label = m.sample()
+    log_prob_c = m.log_prob(pred_label)
+    return pred_label, log_prob_c
+
+def sample_policy_n(ht, policy_n):
+    '''
+    Draw an action from a multinomial distribution specified by policy n.
+    '''
+    action_probs = policy_n(ht)
+    m = Categorical(action_probs)
+    step = m.sample()
+    log_prob_n = m.log_prob(step)
+    return step.item(), log_prob_n
+    
+def compute_policy_value_losses(cost_ep, loss, saved_log_probs, baseline_value_ep, alpha, gamma):
+    '''compute the policy losses and value losses for the current episode
+    '''
+    # normalise cost
+    norm_cost_ep = (cost_ep - np.mean(cost_ep)) / (np.std(cost_ep) + 1e-7)
+    #print('norm_cost_ep:', norm_cost_ep)
+    reward_ep = - alpha * norm_cost_ep
+    reward_ep[-1] -= loss.item()
+    # compute discounted rewards
+    discounted_rewards = [r * gamma ** i for i, r in enumerate(reward_ep)]
+    policy_loss_ep = []
+    value_losses = []
+    for i, log_prob in enumerate(saved_log_probs):
+        # baseline_value_ep[i].item(): updating the policy loss doesn't include the gradient of baseline values
+        advantage = sum(discounted_rewards) - baseline_value_ep[i].item()
+        policy_loss_ep.append(log_prob * advantage)
+        value_losses.append((sum(discounted_rewards) - baseline_value_ep[i]) ** 2)   
+    return policy_loss_ep, value_losses
+
+
+def evaluate(clstm, policy_s, policy_n, policy_c, iterator):
+    '''
+    Evaluate a model with skimming, rereading, and early stopping
+    and compute the average FLOPs per data.
+    '''
+    # set the models in evaluation mode
+    clstm.eval()
+    policy_s.eval()
+    policy_n.eval()
+    policy_c.eval()
+    count_all = 0
+    count_correct = 0
+    start = time.time()
+    # the sum of FLOPs of the iterator set
+    flops_sum = 0
+    with torch.no_grad():
+        for batch in iterator:
+            label = batch.label.to(torch.long)  # for cross entropy loss, the long type is required
+            text = batch.text.view(CHUNCK_SIZE, BATCH_SIZE, CHUNCK_SIZE) # transform 1*400 to 20*1*20
+            curr_step = 0
+            h_0 = torch.zeros([1,1,128]).to(device)
+            count = 0
+            while curr_step < 20 and count < 5: # loop until a text can be classified or currstep is up to 20
+                count += 1
+                # pass the input through cnn-lstm and policy s
+                text_input = text[curr_step] # text_input 1*20
+                ht = clstm(text_input, h_0)  # 1 * 128
+                h_0 = ht.unsqueeze(0)  # 1 * 1 * 128, next input of lstm
+                # draw a stop decision
+                stop_decision, log_prob_s = sample_policy_s(ht, policy_s)
+                flops_sum += clstm_cost + s_cost
+                stop_decision = stop_decision.item()
+                if stop_decision == 1: # classify
+                    break
+                else:
+                    # draw an action (reread or skip)
+                    step, log_prob_n = sample_policy_n(ht, policy_n)
+                    flops_sum += n_cost
+                    curr_step += int(step)  # reread or skip
+            # draw a predicted label
+            output_c = policy_c(ht)
+            flops_sum += c_cost
+            # draw a predicted label 
+            pred_label, log_prob_c = sample_policy_c(output_c)
+            if pred_label.item() == label:
+                count_correct += 1
+            count_all += 1
+    print('Evaluation time elapsed: %.2f s' % (time.time() - start))
+    avg_flop_per_sample = int(flops_sum / len(iterator))
+    print('Average FLOPs per sample: ', avg_flop_per_sample)
+    return count_all, count_correct
+
+
+def evaluate_earlystop(clstm, policy_s, policy_c, iterator):
+    # set the models in evaluation mode
+    clstm.eval()
+    policy_s.eval()
+    policy_c.eval()
+    count_all = 0
+    count_correct = 0
+    start = time.time()
+    # the sum of FLOPs of the iterator set
+    flops_sum = 0
+    with torch.no_grad():
+        for batch in iterator:
+            label = batch.label.to(torch.long) # 64
+            text = batch.text.view(CHUNCK_SIZE, BATCH_SIZE, CHUNCK_SIZE) # transform 1*400 to 20*1*20
+            curr_step = 0
+            # set up the initial input for lstm
+            h_0 = torch.zeros([1,1,128]).to(device) 
+            saved_log_probs = []
+            while (curr_step < 20):
+                '''
+                loop until stop decision equals 1 
+                or the whole text has been read
+                '''
+                # read a chunk
+                text_input = text[curr_step]
+                # hidden state
+                ht = clstm(text_input, h_0)  # 1 * 128
+                h_0 = ht.unsqueeze(0).cuda()  # 1 * 1 * 128, next input of lstm
+                # draw a stop decision
+                stop_decision, log_prob_s = sample_policy_s(ht, policy_s)
+                stop_decision = stop_decision.item()
+                flops_sum += clstm_cost + s_cost
+                if stop_decision == 1:
+                    break
+                else:
+                    curr_step += 1
+            # output of classifier       
+            output_c = policy_c(ht)
+            flops_sum += c_cost
+            # draw a predicted label 
+            pred_label, log_prob_c = sample_policy_c(output_c)
+            if pred_label.item() == label:
+                count_correct += 1
+            count_all += 1     
+    print('Evaluation time elapsed: %.2f s' % (time.time() - start))
+    avg_flop_per_sample = int(flops_sum / len(iterator))
+    print('Average FLOPs per sample: ', avg_flop_per_sample)  
+    return count_all, count_correct
+
+
+def print_model_parm_flops(model, input):
+    '''
+    Compute FLOPs of a model.
+    '''
+    multiply_adds = False
+    list_conv=[]
+    def conv_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+        kernel_ops = self.kernel_size[0] * self.kernel_size[1] * (self.in_channels / self.groups) * (2 if multiply_adds else 1)-1
+        bias_ops = 1 if self.bias is not None else 0
+        params = output_channels * (kernel_ops + bias_ops)
+        flops = batch_size * params * output_height * output_width
+        list_conv.append(flops)
+
+    list_linear=[] 
+    def linear_hook(self, input, output):
+        batch_size = input[0].size(0) if input[0].dim() == 2 else 1
+        weight_ops = self.weight.nelement() * (2 if multiply_adds else 1)
+        bias_ops = self.bias.nelement()
+        flops = batch_size * (weight_ops + bias_ops)
+        list_linear.append(flops)
+
+    list_bn=[] 
+    def bn_hook(self, input, output):
+        list_bn.append(input[0].nelement())
+        
+    list_sig = []
+    def sig_hook(self, input, output):
+        list_sig.append(input[0].nelement())
+    
+    list_softmax = []
+    def softmax_hook(self, input, output):
+        print(input[0].nelement())
+        list_softmax.append(input[0].nelement())
+        
+    list_relu=[] 
+    def relu_hook(self, input, output):
+        list_relu.append(input[0].nelement())
+
+    list_pooling=[]
+    def pooling_hook(self, input, output):
+        batch_size, input_channels, input_height, input_width = input[0].size()
+        output_channels, output_height, output_width = output[0].size()
+        kernel_ops = self.kernel_size * self.kernel_size
+        bias_ops = 0
+        params = output_channels * (kernel_ops + bias_ops)
+        flops = batch_size * params * output_height * output_width
+        list_pooling.append(flops)
+    
+    def foo(net):
+        childrens = list(net.children())
+        if not childrens:
+            if isinstance(net, torch.nn.Conv2d):
+                net.register_forward_hook(conv_hook)
+            if isinstance(net, torch.nn.Linear):
+                net.register_forward_hook(linear_hook)
+            if isinstance(net, torch.nn.BatchNorm2d):
+                net.register_forward_hook(bn_hook)
+            if isinstance(net, torch.nn.ReLU):
+                net.register_forward_hook(relu_hook)
+            if isinstance(net, torch.nn.MaxPool2d) or isinstance(net, torch.nn.AvgPool2d):
+                net.register_forward_hook(pooling_hook)
+            if isinstance(net, torch.nn.Sigmoid):
+                net.register_forward_hook(sig_hook)
+            if isinstance(net, torch.nn.Softmax):
+                net.register_forward_hook(softmax_hook)
+            return
+        for c in childrens:
+                foo(c)
+    foo(model)
+    out = model(input.cuda())
+    total_flops = (sum(list_conv) + sum(list_linear) + sum(list_bn) + sum(list_relu) + sum(list_pooling) + sum(list_sig) +sum(list_softmax)) 
+    return total_flops
+
+
